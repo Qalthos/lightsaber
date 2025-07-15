f46182f245ef7c2018ba712a68ba53ed85002418
Kate Case <this.is@katherineca.se>
2025-07-15 08:26:16 -0400

    Minor work tweaks

--- roles/base/files/config/nvim/lua/config/indent.lua
+++ roles/base/files/config/nvim/lua/config/indent.lua
@@ -5,18 +5,18 @@ vim.cmd("set textwidth=160")
 
 local indent_group = vim.api.nvim_create_augroup("indent", { clear = true })
 vim.api.nvim_create_autocmd(
-  { 'BufNewFile', 'BufRead' },
+  { 'FileType' },
   {
-    pattern = { "*.py", "*.rst" },
+    pattern = { "python", "rst" },
     group = indent_group,
     command = "set tabstop=4 shiftwidth=4 expandtab",
   }
 )
 
 vim.api.nvim_create_autocmd(
-  { 'BufNewFile', 'BufRead' },
+  { 'FileType' },
   {
-    pattern = { "*.css", "*.html", "*.js", "*.lua", "*.yaml", "*.yml" },
+    pattern = { "css", "html", "json", "lua", "spajson", "yaml" },
     group = indent_group,
     command = "set tabstop=2 shiftwidth=2 expandtab",
   }
